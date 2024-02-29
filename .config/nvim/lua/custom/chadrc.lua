c39ecb5915ce1e2f40bcdeb5c48d8fd791f93790
thaddeuscleo <thaddeuscleo@gmail.com>
2024-02-29 17:02:47 +0000

    feat: change statusline

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -7,9 +7,26 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "rosepine",
   theme_toggle = { "rosepine", "one_light" },
-
+  telescope = { style = "bordered" }, -- borderless / bordered
   hl_override = highlights.override,
   hl_add = highlights.add,
+  
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "colored", -- colored / simple
+  },
+
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    overriden_modules = nil,
+  },
+
 }
 
 M.plugins = "custom.plugins"
